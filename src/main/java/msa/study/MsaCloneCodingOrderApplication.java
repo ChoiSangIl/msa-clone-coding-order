efbd0e9d0710ef7bcafd4b7240e4a7b65c344af4
ChoiSangIl <wo566@naver.com>
2022-01-30 00:11:42 +0900

    first commit

---
+++ src/main/java/msa/study/MsaCloneCodingOrderApplication.java
@@ -0,0 +1,13 @@
+package msa.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaCloneCodingOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaCloneCodingOrderApplication.class, args);
+	}
+
+}
